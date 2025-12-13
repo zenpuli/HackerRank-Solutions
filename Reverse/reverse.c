6effb0ab2fb380bb400b639053177c84ba5949f5
zenpuli <zeneswaripuli@gmail.com>
2025-12-13 15:34:36 +0530

    Add array reversal program in reverse.c
    Implement a program to reverse an array of integers.

--- Reverse/reverse.c
+++ Reverse/reverse.c
@@ -1,1 +1,17 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n],reverse[n];
+    for(int i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    int j=0;
+    for(int i=n-1;i>=0;i--){
+        reverse[j]=a[i];
+        j++;
+    }
+    for(int i=0;i<n;i++)
+        printf("%d ",reverse[i]);
+}
