98c102002c224d0275491702fef97ad967f49efd
zenpuli <zeneswaripuli@gmail.com>
2025-12-13 15:35:31 +0530

    Add array reversal functionality in reverse.java
    Implemented a program to reverse an array of integers.

--- Reverse/reverse.java
+++ Reverse/reverse.java
@@ -1,1 +1,22 @@
-
+import java.util.Scanner;
+public class Solution{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        int reverse[]=new int[n];
+        
+        for(int i=0;i<n;i++)
+            a[i]=sc.nextInt();
+            
+        int j=0;
+        for(int i=n-1;i>=0;i--){
+            reverse[j]=a[i];
+            j++;
+        }
+        for(int i=0;i<n;i++)
+            System.out.print(reverse[i]+" ");
+        
+        sc.close();
+    }
+}
