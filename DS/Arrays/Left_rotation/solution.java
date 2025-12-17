5cc081cf9ffd154d8de8f160cc2ae585e16a30f6
zenpuli <zeneswaripuli@gmail.com>
2025-12-18 00:35:01 +0530

    Add left rotation functionality for an array
    Implement left rotation of an array based on user input.

---
+++ DS/Arrays/Left_rotation/solution.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Solution{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int d=sc.nextInt();
+        int a[]=new int[n];
+        int temp=0;
+        for(int i=0;i<n;i++)
+            a[i]=sc.nextInt();
+        
+        while(d>0){
+            temp=a[0];
+            for(int i=0;i<n-1;i++){
+                a[i]=a[i+1];
+            }
+            a[n-1]=temp;
+            d--;
+        }
+        
+        for(int i=0;i<n;i++){
+            System.out.print(a[i]+" ");
+        }
+        sc.close();
+    }
+}
